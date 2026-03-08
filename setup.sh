9ba61c501b2f72ce14b82a04f6e36c13ab324d7f
xadezv <xadezv@users.noreply.github.com>
2026-03-08 14:43:13 +0000

    init

---
+++ setup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt install --no-install-recommends git cmake ninja-build ccache dfu-util device-tree-compiler wget python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file make gcc libsdl2-dev
